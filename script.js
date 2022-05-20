026fec7f703ca3f909090d2eb6b1c7784b29b2d1
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-05-20 18:52:59 -0300

    Added desktop styles. Added a 'show source card' to see a modal with the original print of the card

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const cardsWrapper = document.querySelector(".cards-wrapper");
 const loadMoreButton = document.querySelector(".load-more");
 const buttonScrollUp = document.querySelector(".scroll-to-top");
 const loading = document.querySelector(".loading");
+const modalOuter = document.querySelector(".modal-outer");
+const fullImage = document.querySelector(".full-image");
 buttonScrollUp.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' })  ) ;
 
 function handleError(err) {
@@ -15,7 +17,7 @@ function shuffle(array) {
 }
 function handleScrollUpButton(par) {
     let yPosition = window.scrollY;
-    yPosition > 1500 ? buttonScrollUp.style.opacity = 100 : buttonScrollUp.style.opacity = 0 
+    yPosition > 1000 ? buttonScrollUp.style.opacity = 100 : buttonScrollUp.style.opacity = 0 
 }
 async function dataFetch(endpoint) {
     loading.style.display = "inherit";
@@ -25,26 +27,51 @@ async function dataFetch(endpoint) {
     loading.style.display = "none";
     return data;
 }
+function closeModal() {
+    modalOuter.classList.remove('open');
+}
+function displayFullCard (event) {
+    //MODAL ANDA OK, FALTA PASARLE LA IMAGEN DE LA CARTA ENTERA AL fullImage SRC
+    const fullCardToShow = event.target.value;
+    fullImage.src = fullCardToShow;
+    
+    modalOuter.classList.add('open');
+    modalOuter.addEventListener('click', event => {
+        const clickOutside = !event.target.closest('.full-image');
+        clickOutside ? closeModal() : null;
+    });
+    window.addEventListener('keydown', event => {
+        if (event.key === 'Escape') {
+          closeModal();
+        }
+    });
+
+}
 function displayCard(cardData) {
-    const card = document.createElement("div");
-    card.innerHTML = `
-    <div class="card">
-        <img class="cardArt" src="${cardData.image_uris.art_crop}" alt="">
-        <h1 class="cardName">"${cardData.name}"</h1>
-        <h2 class="cardArtist">${cardData.artist} - ${cardData.frame} </h2>
-        <div class="cardDivider"></div>
-    </div>
+    const cardDiv = document.createElement("div");
+    cardDiv.innerHTML = `
+    <img class="cardArt" src="${cardData.image_uris.art_crop}" alt="">
+    <h1 class="cardName">"${cardData.name}"</h1>
+    <h2 class="cardArtist">${cardData.artist} - ${cardData.frame} </h2>
+    <button class="cardSource" value=${cardData.image_uris.normal}>source card</button>
+    <div class="cardDivider"></div>
     `;
-    cardsWrapper.insertAdjacentElement("beforeend",card);
-    return card;
+    cardDiv.className = "card";
+    const button = cardDiv.childNodes[7];
+    const fullCardUrl = cardData.image_uris.normal;
+    button.addEventListener('click', event => displayFullCard(event));
+    cardsWrapper.insertAdjacentElement("beforeend",cardDiv);
+    return cardDiv;
 }
 async function displayMore(nextEndpoint) {
+    loadMoreButton.style.display = "none";
     if (nextEndpoint !== "undefined") {
         const cardsList = await dataFetch(nextEndpoint);
         const cardsArray = cardsList.data;
         cardsArray.forEach( (card) =>  card.image_uris !== undefined ? displayCard(card) : null);
         const nextPageEndpoint = cardsList.next_page;
         loadMoreButton.value = nextPageEndpoint;
+        loadMoreButton.style.display = "initial";
         return;
     } else {
         loadMoreButton.style.display = "none";
